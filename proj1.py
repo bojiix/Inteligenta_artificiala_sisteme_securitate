e45b3c404b543975eb72b2f328e26cd285536ee0
None <None>
2022-01-18 14:04:06 -0800

    first commit

---
+++ proj1.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+from sys import argv
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
+import time
+import os
+
+class SingleSwitchWithNHostsTopo( Topo ):
+  def build( self, n=3 ):
+    switch = self.addSwitch('s1')
+    for h in range(n):
+      print('h%s' % (h + 1))
+      host = self.addHost('h%s' % (h + 1))
+      self.addLink(host, switch, bw=1, loss=0)
+
+def generateICMPTraffic(h1, h3):
+  h1_IP = str(h1.IP())
+
+  print('Traffic capture incoming...')
+
+  h3.cmd('tcpdump -i %s "icmp[0] == 8" -w ./pcap/attack.pcap &' % h3.intfNames()[0])
+
+  print('DOS running...')
+  h3.cmd('ping %s &' % h1_IP)
+
+  time.sleep(10)
+
+  h3.cmd('killall ping')
+  h3.cmd('killall tcpdump')
+  print('Attack traffic captured.')
+
+def runRegularTraffic(h1, h2):
+  h1.cmd('iperf -s &')
+  h2.cmd('iperf -c 10.0.0.1 -t 100 &')
+
+def runAttackTraffic(h3):
+  print('Commencing attack...')
+  h3.cmd('tcpreplay -i %s -t -l 10000 ./pcap/attack.pcap &' % h3.intfNames()[0])
+
+  time.sleep(20)
+
+  h3.cmd('killall tcpreplay')
+  print('End of attack.')
+
+def toggleCapture(h1, h2, h3, stop = False):
+  if stop == True:
+    print('Stop final commands.')
+    h1.cmd('killall tcpdump')
+    h2.cmd('killall tcpdump')
+    h3.cmd('killall tcpdump')
+    h1.cmd('killall iperf')
+    h2.cmd('killall iperf')
+  else:
+    print('Start final commands...')
+    h1.cmd('tcpdump -i %s -w ./pcap/h1.pcap &' % h1.intfNames()[0])
+    h2.cmd('tcpdump -i %s -w ./pcap/h2.pcap &' % h2.intfNames()[0])
+    h3.cmd('tcpdump -i %s -w ./pcap/h3.pcap &' % h3.intfNames()[0])
+
+def start():
+  print('Creating topology...')
+  topo = SingleSwitchWithNHostsTopo()
+  net = Mininet( topo=topo,
+                 host=CPULimitedHost, link=TCLink,
+                 autoStaticArp=True )
+  net.start()
+
+  h1 = net.get('h1')
+  h2 = net.get('h2')
+  h3 = net.get('h3')
+
+  if(len(h1.intfNames()) <= 0 or len(h2.intfNames()) <= 0 or len(h3.intfNames()) <= 0):
+    print('Weird, empty interface list')
+    return
+
+  generateICMPTraffic(h1, h3)
+  toggleCapture(h1, h2, h3)
+  runRegularTraffic(h1, h2)
+  time.sleep(10)
+  runAttackTraffic(h3)
+  time.sleep(20)
+  toggleCapture(h1, h2, h3, True)
+  net.stop()
+  print('Net stop!')
+
+def preparePcapFolder():
+  os.system('rm -rf pcap')
+  os.system('mkdir pcap')
+  os.system('touch ./pcap/h1.pcap')
+  os.system('touch ./pcap/h2.pcap')
+  os.system('touch ./pcap/h3.pcap')
+  os.system('touch ./pcap/attack.pcap')
+
+if __name__ == '__main__':
+  setLogLevel( 'info' )
+  preparePcapFolder()
+  start()
